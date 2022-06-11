3eab3a59785b35311d277607d1837cb8176eb5bd
rahulchalana <84458888+rahulchalana@users.noreply.github.com>
2022-06-11 16:15:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+
+document.querySelector(".w").addEventListener("click", function(){
+    this.style.color= "white";
+    var tom_1= new Audio('sounds/tom-1.mp3');
+    tom_1.play();
+});
+document.querySelector(".a").addEventListener("click", function(){
+    this.style.color= "white";
+    var tom_2= new Audio('sounds/tom-2.mp3');
+    tom_2.play();
+});
+document.querySelector(".s").addEventListener("click", function(){
+    this.style.color= "white";
+    var tom_3= new Audio('sounds/tom-3.mp3');
+    tom_3.play();
+});
+document.querySelector(".d").addEventListener("click", function(){
+    this.style.color= "white";
+    var tom_4= new Audio('sounds/tom-4.mp3');
+    tom_4.play();
+});
+document.querySelector(".j").addEventListener("click", function(){
+    this.style.color= "white";
+    var kick_bass= new Audio('sounds/kick-bass.mp3');
+    kick_bass.play();
+});
+document.querySelector(".k").addEventListener("click", function(){
+    this.style.color= "white";
+    var snare= new Audio('sounds/snare.mp3');
+    snare.play();
+});
+document.querySelector(".l").addEventListener("click", function(){
+    this.style.color= "white";
+    var crash= new Audio('sounds/crash.mp3');
+    crash.play();
+});
+
+
